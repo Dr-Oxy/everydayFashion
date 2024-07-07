a67a1d80607cf7be2a0f430becde15cd99af0c12
droxy <chiomaotu22@gmail.com>
2024-07-07 13:53:59 +0100

    created useProduct hook

---
+++ hooks/useProducts.ts
@@ -0,0 +1,19 @@
+import { axiosRequest } from '@/utils/axiosConfig';
+import { useQuery } from '@tanstack/react-query';
+
+//define env
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+const orgId = process.env.EXPO_PUBLIC_ORGID;
+const appId = process.env.EXPO_PUBLIC_APPID;
+
+export const useGetProducts = () => {
+  const query = useQuery({
+    queryKey: ['products'],
+    queryFn: () =>
+      axiosRequest().get(
+        `/products?organization_id=${orgId}&reverse_sort=false&size=10&Appid=${appId}&Apikey=${apiKey}`,
+      ),
+  });
+
+  return query;
+};
